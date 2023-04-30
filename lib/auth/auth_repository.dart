ab5d2b9c971508ca43bf3ab990936b23b2f0c245
LysenkoWork <lyss.work@gmail.com>
2023-04-30 22:29:01 +0300

    auth

--- lib/auth/auth_repository.dart
+++ lib/auth/auth_repository.dart
@@ -3,7 +3,72 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:science_art/model/models.dart';
 
+class Reiting {
+  String? id;
+  String? ballov;
+
+  Reiting({this.id, this.ballov});
+
+  Reiting.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    ballov = json['ballov'];
+  }
+/*
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['ballov'] = ballov;
+  }
+*/
+}
+
+class ReitingUser {
+  String? cid;
+  String? uid;
+  String? ballov;
+
+  ReitingUser({this.cid, this.uid, this.ballov});
+
+  ReitingUser.fromJson(Map<String, dynamic> json) {
+    cid = json['cid'];
+    uid = json['uid'];
+    ballov = json['ballov'];
+  }
+}
+
 class AuthRepository {
+
+  Future<Reiting> getReiting({required String id}) async {
+    String url = 'http://science-art.pro/reiting.php';
+    Response response = await post(Uri.parse(url), body: {'id': id});
+    if (response.statusCode == 200) {
+      if (response.body != 'null') {
+        final Reiting reiting = Reiting.fromJson(json.decode(response.body));
+        return reiting;
+      }
+    }
+    return Reiting();
+  }
+
+  Future<ReitingUser> getReitingUser({required String cid, required String uid}) async {
+    String url = 'http://science-art.pro/reitinguser.php';
+    Response response = await post(Uri.parse(url), body: {'cid': cid, 'uid': uid});
+    if (response.statusCode == 200) {
+      if (response.body != 'null') {
+        final ReitingUser reitinguser = ReitingUser.fromJson(json.decode(response.body));
+        return reitinguser;
+      }
+    }
+    return ReitingUser();
+  }
+
+  Future<void> addReiting({required String cid, required String uid, required String ballov}) async {
+    String url = 'http://science-art.pro/addreiting.php';
+    Response response = await post(Uri.parse(url), body: {'cid': cid, 'uid': uid, 'ballov': ballov});
+    if (response.statusCode != 200) {
+      }
+  }
+
   Future<User> signUp({required String user, required String password}) async {
     String url = 'http://science-art.pro/signup.php';
     Response response = await post(Uri.parse(url), body: {'name': user});
@@ -39,3 +104,6 @@ class AuthRepository {
     }
   }
 }
+
+
+
